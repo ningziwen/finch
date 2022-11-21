5829d70df6684a3283d453d24b87767ba49f11b8
Ziwen Ning <ningziwe@amazon.com>
2022-11-21 09:47:20 -0800

    simplify-test

--- e2e/version_test.go
+++ e2e/version_test.go
@@ -4,8 +4,12 @@
 package e2e
 
 import (
+	"os/exec"
+	"time"
+
 	"github.com/onsi/ginkgo/v2"
-	"github.com/runfinch/common-tests/command"
+	"github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 	"github.com/runfinch/common-tests/option"
 )
 
@@ -13,8 +17,12 @@ var testVersion = func(o *option.Option) {
 	ginkgo.Context("Version", func() {
 		ginkgo.Specify("Test version", func() {
 			//exec.Command("finch", "version").Run()
-			command.New(o, "version")
-			//command.Run(o, "version")
+			cmd := exec.Command("finch", "version") //nolint:gosec // G204 is not an issue because cmdName is fully controlled by the user.
+			cmd.Stdin = nil
+			session, err := gexec.Start(cmd, ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
+			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			session.Wait(10 * time.Second)
+			gomega.Expect(session).Should(gexec.Exit(0))
 			//gomega.Expect(command.StdoutStr(o, "version")).To(gomega.Equal(fmt.Sprintf("Finch version: %s", "v0.1.0")))
 		})
 	})
