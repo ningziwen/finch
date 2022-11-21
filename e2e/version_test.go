27530983b1bc73d15b5f3e606b2e680338113c33
Ziwen Ning <ningziwe@amazon.com>
2022-11-21 09:31:26 -0800

    test

--- e2e/version_test.go
+++ e2e/version_test.go
@@ -4,16 +4,16 @@
 package e2e
 
 import (
-	"os/exec"
-
 	"github.com/onsi/ginkgo/v2"
+	"github.com/runfinch/common-tests/command"
 	"github.com/runfinch/common-tests/option"
 )
 
 var testVersion = func(o *option.Option) {
 	ginkgo.Context("Version", func() {
 		ginkgo.Specify("Test version", func() {
-			exec.Command("finch", "version").Run()
+			//exec.Command("finch", "version").Run()
+			command.Run(o, "version")
 			//gomega.Expect(command.StdoutStr(o, "version")).To(gomega.Equal(fmt.Sprintf("Finch version: %s", "v0.1.0")))
 		})
 	})
